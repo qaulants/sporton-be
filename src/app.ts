e1683f9219b8e083f643c50f0faeea97dc636791
Qaulan Tsaqila <qaulantsaqila75@gmail.com>
2026-02-04 16:18:03 +0700

    Session 8 update

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes";
 import categoryRoutes from "./routes/category.routes";
 import productRoutes from "./routes/product.routes";
+import bankRoutes from "./routes/bank.routes"
+import transactionRoutes from "./routes/transaction.routes"
 import { authenticate } from "./middlewares/auth.middleware";
 import path from "path";
 
@@ -17,6 +19,8 @@ app.use("/api/auth", authRoutes);
 // saat mengakses signin  juga melalui /api/auth/signin
 app.use("/api/categories", categoryRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/banks", bankRoutes);
+app.use("/api/transactions", transactionRoutes);
 
 app.get("/", (req, res) => {
   res.send("Sporton Backend API is Running");
